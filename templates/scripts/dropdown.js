dc7f2fb9976158834febc5a3c019836435f2894f
ricke1b <70524640+ricke1b@users.noreply.github.com>
2020-11-11 22:12:29 -0500

    Added dropdown menus to select course, date and time with a button to output the selected data for validation. These are just placeholders for now and the information will be pulled from the database once it is implemented.

--- templates/scripts/dropdown.js
+++ templates/scripts/dropdown.js
@@ -1,19 +1,13 @@
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
+/*When the user clicks Search, display the selected data*/ 
+var displayData = function() {
+  var course = document.getElementById("courses").value;
+  var date = document.getElementById("dates").value;
+  var time = document.getElementById("times").value;
+  document.getElementById("output").innerHTML = "Course selected: " + course 
+  + "<br>" + "Date selected: " + date
+  + "<br>" + "Time selected: " + time;
 }
 
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
+window.onload = function(){
+  document.getElementById("search").onclick = displayData;
 }
