a191309feadb952ad5a4b5b59d4f1d4bacff20ac
Rachel Crowley <crowleyrr@gmail.com>
2020-11-13 01:10:38 -0500

    Added navigation from home page to all subpages

--- users/urls.py
+++ users/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     url(r'^recommend/$', TemplateView.as_view(template_name="Recommend.html")),
     url(r'^schedule/$', TemplateView.as_view(template_name="Schedule.html")),
     url(r'^search/$', TemplateView.as_view(template_name="Search.html")),
-
+    url(r'^signin/$', TemplateView.as_view(template_name="Sign_In.html")),
 
 ]
